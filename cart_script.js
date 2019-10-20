eefea035b239e4258379c80104b754764ac3d624
Kiranendra <vivs8899@gmail.com>
2019-10-20 09:13:15 +0530

    Seven
    Still to add purchase button, remove button and function to clear local storage.

--- cart_script.js
+++ cart_script.js
@@ -2,14 +2,41 @@ var cart = angular.module('myCart', []);
 var cart_Items = JSON.parse(window.localStorage.getItem("cartItems"));
 console.log(JSON.parse(window.localStorage.getItem("cartItems")));
 
+if (cart_Items.length == 0) {
+   alert("Your cart is empty!");
+}
+
 cart.controller('cartPriceCtrl',function($scope) {
    var total = 0;
-   for(var i = 0, max = cart_Items.length; i < max; i++){
-      total += cart_Items[i].itemprice;
+   if(cart_Items != null){
+      for(var i = 0, max = cart_Items.length; i < max; i++){
+         total += cart_Items[i].itemprice;
+      }
+      $scope.cart_total_price = total;
    }
-   $scope.cart_total_price = total;
+   else {
+      $scope.cart_total_price = 0;
+   }
 });
 
 cart.controller('itemsDisplayCtrl',function($scope) {
    $scope.itemsInCart = cart_Items;
+   $scope.itemsDetails = [{
+                name: 'Batman',
+                universe: 'DC',
+            }, {
+                name: 'Ant Man',
+                universe: 'Marvel',
+            }, {
+                name: 'Superman',
+                universe: 'DC',
+            }, {
+                name: 'Captain America',
+                universe: 'Marvel',
+            }, {
+                name: 'Thor',
+                universe: 'Marvel',
+            }
+
+        ];
 });
